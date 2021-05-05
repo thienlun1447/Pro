e4ba19ade62beb59be0b139a31ad1876221fcd45
thienlun1447 <83632138+thienlun1447@users.noreply.github.com>
2021-05-05 08:52:09 +0700

    Update invoice_create.php

--- ATN/invoice_create.php
+++ ATN/invoice_create.php
@@ -27,11 +27,11 @@
             ".$invoice_product_quantity[$i]."</p>";
         }
 
-        $host="ec2-3-233-43-103.compute-1.amazonaws.com";
-        $database="daoeb2h1i4gref";
-        $user = "ljguzytsklzppr";
+        $host="ec2-18-233-83-165.compute-1.amazonaws.com";
+        $database="db24t10uekpnvh";
+        $user = "jbuuiblgavnapv";
         $port = "5432";
-        $password = "88ba687e8bbe5cbd0741417086045a25cc5bb0d1b3d35ab35cfea4c48680e1fa";
+        $password = "466bceba4becfc6ff658d160e30545aa26cc46dce8fe63d75b63453dc36a5325";
 
         $host_param_str = "host=".$host;
         $dbname_param_str = "dbname=".$database;
@@ -51,7 +51,7 @@
         }else{
             echo "SUCCESS: Connection to Heroku Postgres has been established";
 
-            $product_query = 'INSERT INTO public."Customer"(Email,Id,Name,Phone,Address) 
+            $product_query = 'INSERT INTO public."CUSTOMER"(Email,Id,Name,Phone,Address) 
             VALUES (\''.$cusid.'\',\''.$cusname.'\',\''."Default".'\',\''."Default".'\','."Default".')';
 
             echo '<p>' .$product_query. '</p>';
