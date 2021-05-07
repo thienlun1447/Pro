b8ce63d5516587fe144e23eda086a0c93639613b
thienlun1447 <83632138+thienlun1447@users.noreply.github.com>
2021-05-07 10:45:08 +0700

    s

--- ATN/invoice_create.php
+++ ATN/invoice_create.php
@@ -1,67 +1,65 @@
-<html>
-    <body>
-        <h1>Sale revenue by Category</h1>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
 
-    <?php
-    echo "<p>This is the invoice submssion<p>";
-    ?>
+<body>
+            <?php
+            $customerid = $_REQUEST["customer_id"];
+            $customername = $_REQUEST["customer_name"];
+            $invoiceid = $_REQUEST["invoice_id"];
+            $invoicedate = $_REQUEST["invoice_date"];
+            $productid = $_REQUEST["invoice_product_id"];
+            $quantity = $_REQUEST["invoice_product_quantity"];
 
-    <?php
-        $cusid = $_REQUEST["customer_id"];
-        $cusname = $_REQUEST["customer_name"];
-
-        $invoiceid = $_REQUEST["invoice_id"];
-        $invoicedate = $_REQUEST["invoice_date"];
-        
-        $invoice_product_id = $_REQUEST["invoice_productid"];
-        $invoice_product_quantity = $_REQUEST["invoice_product_quantity"];
-
-        echo "<p>".$cusid. "</p>";
-        echo "<p>".$cusname. "</p>";
-        echo "<p>".$invoiceid. "</p>";
-        echo "<p>".$invoicedate. "</p>";
-
-        for ($i = 0; $i < count($invoice_product_id); $i++){
-            echo "<p>". $invoice_product_id[$i]."
-            ".$invoice_product_quantity[$i]."</p>";
-        }
-
-        $host="ec2-18-233-83-165.compute-1.amazonaws.com";
-        $database="db24t10uekpnvh";
-        $user = "jbuuiblgavnapv";
-        $port = "5432";
-        $password = "466bceba4becfc6ff658d160e30545aa26cc46dce8fe63d75b63453dc36a5325";
-
-        $host_param_str = "host=".$host;
-        $dbname_param_str = "dbname=".$database;
-        $port_param_str = "port=".$port;
-        $user_param_str = "user=".$user;
-        $pass_param_str = "password=".$password;
-        $sslmode_param_str = "sslmode= require";
-
-        $connection_string = $host_param_str.$dbname_param_str.$port_param_str.$user_param_str.$pass_param_str.$sslmode_param_str;
-        
-        echo "<p>".$connection_string."</p>";
-        
-        $connetion = pg_connect($connection_string);
-        
-        if ($connection === false){
-            die("ERROR: Could not connect to the database");
-        }else{
-            echo "SUCCESS: Connection to Heroku Postgres has been established";
-
-            $product_query = 'INSERT INTO public."CUSTOMER"("Email","Id","Name","Phone","Address") 
-            VALUES (\''.$cusid.'\',\''.$cusname.'\',\''."Default".'\',\''."Default".'\','."Default".')';
-
-            echo '<p>' .$product_query. '</p>';
+            echo "<p>".$customerid."</p>";
+            echo "<p>".$customername."</p>";
+            echo "<p>".$invoiceid."</p>";
+            echo "<p>".$invoicedate."</p>";
 
 
-		    if (pg_query($connection,$product_query)){
-			    echo '<p>SUCCESS: Record is added succesfully. A new product is created</p>';
-		    }else{
-			    echo '<p>ERROR: Could not execute query</p>';
-		    }
-        }
-    ?>
+            for($i = 0; $i < count($productid); $i++){
+                echo "<p>".$productid[$i]."_".$quantity[$i]."</p>";
+            }
+            $sum_id = "";
+            $sum_qua ="";
+            $sum = 'INSERT INTO public."invoice"("Customerid","Customername","Invoiceid","Invoicedate","Productid","Quantity") VALUES (\''.$customerid.'\',\''.$customername.'\',\''.$invoiceid.'\',\''.$invoicedate.'\',\'{"'.$productid[0].'"';
+
+            for($i = 1; $i < count($productid); $i++){
+                $sum_id = ',"'.$productid[$i].'"';
+                $sum = $sum.$sum_id;        
+            }
+            $sum = $sum.'}\',\'{"'.$quantity[0].'"';
+
+
+            for($i = 1; $i < count($quantity); $i++){
+                $sum_qua = ',"'.$quantity[$i].'"';
+                $sum = $sum.$sum_qua;
+            }
+            $sum = $sum.'}\')';
+            echo "<p>".$sum."</p>";
+
+
+            $host = "ec2-3-234-85-177.compute-1.amazonaws.com";
+            $database = "d4sbqi18abljpu";
+            $user = "qhgbytlxmnxdsc";
+            $port = "5432";
+            $password = "51a7b6c1f816cab155a988a6e8f482dabfff5f18ea87d416f14dfd72b91a8ee8";
+
+            $connection_string = "host=".$host." dbname=".$database." user=".$user." port=".$port." password=".$password." sslmode=require";
+            $connection = pg_connect($connection_string);
+            if ($connection === false) {
+                die ("ERROR: Could not connect to database");
+            } 
+            else {
+                echo "SUCCESS: Connection to Heroku Postgres has been established";
+
+
+                if (pg_query($connection, $sum)) {
+                    echo '<p>SUCCESS: Record is added successfully. A new customer is created</p>';
+                }
+                else {
+                    echo '<p>ERROR: Could not execute query</p>';
+                }
+            }
+            ?>
+</body>
 </html>
