9e93346adc9e6932483a13f134dd370dc758afd0
thienlun1447 <83632138+thienlun1447@users.noreply.github.com>
2021-05-07 19:32:01 +0700

    q

--- ATN/invoice_create.php
+++ ATN/invoice_create.php
@@ -1,61 +1,72 @@
-<body>
+<!DOCTYPE html>
+<html>
 <?php
-        $customerid = $_REQUEST["customer_id"];
-        $customername = $_REQUEST["customer_name"];
+    echo "This is the invoice submssion";
+    ?>
+
+    <?php
+        $cusid = $_REQUEST["customer_id"];
+        $cusname = $_REQUEST["customer_name"];
+
         $invoiceid = $_REQUEST["invoice_id"];
         $invoicedate = $_REQUEST["invoice_date"];
-        $productid = $_REQUEST["invoice_product_id"];
-        $quantity = $_REQUEST["invoice_product_quantity"];
+        
+        $invoice_product_id = $_REQUEST["invoice_product_id"];
+        $invoice_product_quantity = $_REQUEST["invoice_product_quantity"];
 
-        echo "<p>".$customerid."</p>";
-        echo "<p>".$customername."</p>";
+        echo "<p>".$cusid."</p>";
+        echo "<p>".$cusname."</p>";
         echo "<p>".$invoiceid."</p>";
         echo "<p>".$invoicedate."</p>";
+        echo "<p>".$invoice_product_id."</p>";
+        echo "<p>".$invoice_product_quantity."</p>";
 
-
-        for($i = 0; $i < count($productid); $i++){
-            echo "<p>".$productid[$i]."_".$quantity[$i]."</p>";
+        $product_id = "";
+        for ($i = 0; $i < count($invoice_product_id); $i++){
+            $product_id .= $invoice_product_id[$i].",";
         }
-        $sum_id = "";
-        $sum_qua ="";
-        $sum = 'INSERT INTO public."Invoice"("Customerid","Customername","Invoiceid","Invoicedate","Productid","Quantity") VALUES (\''.$customerid.'\',\''.$customername.'\',\''.$invoiceid.'\',\''.$invoicedate.'\',\'{"'.$productid[0].'"';
 
-        for($i = 1; $i < count($productid); $i++){
-            $sum_id = ',"'.$productid[$i].'"';
-            $sum = $sum.$sum_id;        
+        $product_quantity = "";
+        for ($i = 0; $i < count($invoice_product_quantity); $i++){
+            $product_quantity .= $invoice_product_quantity[$i].",";
         }
-        $sum = $sum.'}\',\'{"'.$quantity[0].'"';
-        
 
-        for($i = 1; $i < count($quantity); $i++){
-            $sum_qua = ',"'.$quantity[$i].'"';
-            $sum = $sum.$sum_qua;
-        }
-        $sum = $sum.'}\')';
-        
-
-        $host = "ec2-18-233-83-165.compute-1.amazonaws.com";
-        $database = "db24t10uekpnvh";
-        $user = "jbuuiblgavnapv";
+        $host="ec2-3-233-43-103.compute-1.amazonaws.com";
+        $database="daoeb2h1i4gref";
+        $user = "ljguzytsklzppr";
         $port = "5432";
-        $password = "466bceba4becfc6ff658d160e30545aa26cc46dce8fe63d75b63453dc36a5325";
+        $password = "88ba687e8bbe5cbd0741417086045a25cc5bb0d1b3d35ab35cfea4c48680e1fa";
 
-        $connection_string = "host=".$host." dbname=".$database." user=".$user." port=".$port." password=".$password." sslmode=require";
+        $host_param_str = "host=".$host;
+	    $dbname_param_str = " dbname=".$database;
+	    $port_param_str = " port=".$port;
+	    $user_param_str = " user=".$user;
+	    $pass_param_str = " password=".$password;
+	    $sslmode_param_str = " sslmode=require";
+
+        $connection_string = $host_param_str.$dbname_param_str.$port_param_str.$user_param_str.$pass_param_str.$sslmode_param_str;
+
+        
+        
         $connection = pg_connect($connection_string);
-        if ($connection === false) {
-            die ("ERROR: Could not connect to database");
-        } 
-        else {
+        
+        if ($connection === false){
+            die("ERROR: Could not connect to the database");
+        }else{
             echo "SUCCESS: Connection to Heroku Postgres has been established";
-            
-            echo "<p>".$sum."</p>";
 
-            if (pg_query($connection, $sum)) {
-                echo '<p>SUCCESS: Record is added successfully. A new customer is created</p>';
-            }
-            else {
-                echo '<p>ERROR: Could not execute query</p>';
-            }
+            $invoice_query = 'INSERT INTO public."invoice"("customer_id" , "customer_name" , "invoice_id" , "invoice_date" , "invoice_product_id" ,"invoice_product_quantity") VALUES 
+            (\''.$cusid.'\',\''.$cusname.'\',\''.$invoiceid.'\',\''.$invoicedate.'\',\''.$invoice_product_id[0].'\',\''.$invoice_product_quantity[0].'\')';
+            
+            echo '<p>'.$invoice_query.'</p>';
+
+
+		    if (pg_query($connection,$invoice_query)){
+			    echo '<p>SUCCESS: Record is added succesfully. A new product is created</p>';
+		    }else{
+			    echo '<p>ERROR: Could not execute query</p>';
+		    }
         }
-?>
-</body>
+    ?>
+    
+</html>
